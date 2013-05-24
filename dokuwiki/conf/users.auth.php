8cf4174db805bac3c3db594cb660a955594705fa
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2013-05-25 00:32:38 +0100

    Replace dokuwiki submodule with dokuwiki local files

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+eechris:$1$qqkbTkDq$0WU2Zk7u0EQsApdBuf1wy1:Chris Jobling:c.p.jobling@swansea.ac.uk:admin,user
