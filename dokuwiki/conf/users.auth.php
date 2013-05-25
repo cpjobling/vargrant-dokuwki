cf5daede1479c52e9fdfdc13b180f0f1cc1642e0
Chris Jobling <C.P.Jobling@Swansea.ac.uk>
2013-05-25 09:45:16 +0100

    Anonomize admin user

--- dokuwiki/conf/users.auth.php
+++ dokuwiki/conf/users.auth.php
@@ -9,4 +9,4 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-eechris:$1$qqkbTkDq$0WU2Zk7u0EQsApdBuf1wy1:Chris Jobling:c.p.jobling@swansea.ac.uk:admin,user
+admin:$1$z70pec2U$Vse8pgrZhqMS0Y197GOcq/:Dokuwiki Administrator:example@example.com:admin,user
